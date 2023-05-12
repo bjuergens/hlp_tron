bccb69c17a678254c45d09a5d439a4ae46a0dbfd
Your Name <you@example.com>
2023-05-12 19:00:03 +0200

    v2

--- tron.js
+++ tron.js
@@ -1,59 +1,120 @@
-const canvas = document.getElementById('tron');
-const ctx = canvas.getContext('2d');
-const gridSize = 10;
-let grid = [];
+let canvas = document.getElementById('game-board');
+let context = canvas.getContext('2d');
+let gridSize = 20;
+let direction = 'right';
+let player;
+let gameLoop;
 
-const player1 = { x: 50, y: 50, dx: 1, dy: 0, color: 'blue' };
-const player2 = { x: 250, y: 250, dx: -1, dy: 0, color: 'red' };
+// Player initialization
+function startGame() {
+    player = {
+        x: canvas.width / 2,
+        y: canvas.height / 2,
+        width: gridSize,
+        height: gridSize,
+        color: '#0f0',
+        dy: 0,
+        dx: gridSize,
+        trail: [],
+        speed: 100
+    };
 
-function resetGame() {
-  grid = [];
-  for (let i = 0; i < canvas.width / gridSize; i++) {
-    grid[i] = [];
-  }
-  player1.x = 50;
-  player1.y = 50;
-  player1.dx = 1;
-  player1.dy = 0;
-
-  player2.x = 250;
-  player2.y = 250;
-  player2.dx = -1;
-  player2.dy = 0;
-
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
+    document.getElementById('start-game').style.display = 'none';
+    direction = 'right';
+    if(gameLoop) clearInterval(gameLoop);
+    gameLoop = setInterval(draw, player.speed);
 }
 
-function drawPlayer(player) {
-  ctx.fillStyle = player.color;
-  ctx.fillRect(player.x, player.y, gridSize, gridSize);
-  player.x += player.dx * gridSize;
-  player.y += player.dy * gridSize;
+// Draw game
+function draw() {
+    context.clearRect(0, 0, canvas.width, canvas.height);
 
-  if (player.x < 0 || player.x >= canvas.width || player.y < 0 || player.y >= canvas.height || grid[player.x / gridSize][player.y / gridSize]) {
-    console.log(player.color + ' hat verloren!');
-    resetGame();
-  } else {
-    grid[player.x / gridSize][player.y / gridSize] = true;
-  }
+    player.x += player.dx;
+    player.y += player.dy;
+
+    // Acceleration when close to wall
+    let distanceToWall;
+    switch (direction) {
+        case 'up': distanceToWall = player.y; break;
+        case 'down': distanceToWall = canvas.height - player.y; break;
+        case 'left': distanceToWall = player.x; break;
+        case 'right': distanceToWall = canvas.width - player.x; break;
+    }
+    if (distanceToWall < 100) {
+        player.speed = 50;
+    } else {
+        player.speed = 100;
+    }
+
+    // Game over when player hits the edge
+    if (player.x < 0 || player.y < 0 || player.x > canvas.width || player.y > canvas.height) {
+        endGame();
+    }
+
+    // Game over when player collides with trail
+    for (let i = 0; i < player.trail.length; i++) {
+        if (player.x === player.trail[i].x && player.y === player.trail[i].y) {
+            endGame();
+        }
+    }
+
+    // Add new position to trail
+    player.trail.push({x: player.x, y: player.y});
+
+    // Draw player
+    context.fillStyle = player.color;
+    context.fillRect(player.x, player.y, player.width, player.height);
+
+    // Limit trail length
+    while (player.trail.length > 100) {
+        player.trail.shift();
+    }
 }
 
-const gameInterval = setInterval(gameLoop, 100);
-
-function gameLoop() {
-  drawPlayer(player1);
-  drawPlayer(player2);
-}
-
+// Update player direction
 window.addEventListener('keydown', function(e) {
-  switch(e.key) {
-    case 'w': player1.dx = 0; player1.dy = -1; break;
-    case 'a': player1.dx = -1; player1.dy = 0; break;
-    case 's': player1.dx = 0; player1.dy = 1; break;
-    case 'd': player1.dx = 1; player1.dy = 0; break;
-    case 'ArrowUp': player2.dx = 0; player2.dy = -1; break;
-    case 'ArrowLeft': player2.dx = -1; player2.dy = 0; break;
-    case 'ArrowDown': player2.dx = 0; player2.dy = 1; break;
-    case 'ArrowRight': player2.dx = 1; player2.dy = 0; break;
-  }
+    switch (e.key) {
+        case 'ArrowUp':
+            if (direction !== 'down') {
+                direction = 'up';
+                player.dy = -gridSize;
+                player.dx = 0;
+            }
+            break;
+        case 'ArrowDown':
+            if (direction !== 'up') {
+                direction = 'down';
+                player.dy = gridSize;
+                player.dx = 0;
+            }
+            break;
+        case 'ArrowLeft':
+            if (direction !== 'right') {
+                direction = 'left';
+                player.dy = 0;
+                player.dx = -gridSize;
+            }
+            break;
+        case 'ArrowRight':
+            if (direction !== 'left') {
+                direction = 'right';
+                player.dy = 0;
+                player.dx = gridSize;
+            }
+            break;
+    }
 });
+
+// End game
+
+function endGame() {
+  clearInterval(gameLoop);
+  document.getElementById('start-game').style.display = 'block';
+  alert("Game Over!");
+}
+
+// Start game button
+document.getElementById('start-game').addEventListener('click', startGame);
+
+// Call startGame to start the game
+startGame();
