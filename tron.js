fb96e0e807c833ed00b92c7506430b25871d6f7d
Your Name <you@example.com>
2023-05-12 19:05:00 +0200

    add player2

--- tron.js
+++ tron.js
@@ -1,14 +1,15 @@
 let canvas = document.getElementById('game-board');
 let context = canvas.getContext('2d');
 let gridSize = 20;
-let direction = 'right';
-let player;
+let direction1 = 'right';
+let direction2 = 'up';
+let player1, player2;
 let gameLoop;
 
-// Player initialization
+// Players initialization
 function startGame() {
-    player = {
-        x: canvas.width / 2,
+    player1 = {
+        x: canvas.width / 4,
         y: canvas.height / 2,
         width: gridSize,
         height: gridSize,
@@ -19,110 +20,168 @@ function startGame() {
         speed: 100
     };
 
+    player2 = {
+        x: (canvas.width / 4) * 3,
+        y: canvas.height / 2,
+        width: gridSize,
+        height: gridSize,
+        color: '#f00',
+        dy: -gridSize,
+        dx: 0,
+        trail: [],
+        speed: 100
+    };
+
     document.getElementById('start-game').style.display = 'none';
-    direction = 'right';
+    direction1 = 'right';
+    direction2 = 'up';
     if(gameLoop) clearInterval(gameLoop);
-    gameLoop = setInterval(draw, player.speed);
+    gameLoop = setInterval(draw, player1.speed); // Assume both players have the same speed
 }
 
 // Draw game
-
-
-
 function draw() {
-  context.clearRect(0, 0, canvas.width, canvas.height);
+    context.clearRect(0, 0, canvas.width, canvas.height);
 
-  player.x += player.dx;
-  player.y += player.dy;
+    updatePlayer(player1, direction1);
+    updatePlayer(player2, direction2);
 
-  // Acceleration when close to wall
-  let distanceToWall;
-  switch (direction) {
-      case 'up': distanceToWall = player.y; break;
-      case 'down': distanceToWall = canvas.height - player.y; break;
-      case 'left': distanceToWall = player.x; break;
-      case 'right': distanceToWall = canvas.width - player.x; break;
-  }
-  if (distanceToWall < 100) {
-      player.speed = 50;
-  } else {
-      player.speed = 100;
-  }
-
-  // Game over when player hits the edge
-  if (player.x < 0 || player.y < 0 || player.x > canvas.width || player.y > canvas.height) {
-      endGame();
-  }
-
-  // Game over when player collides with trail
-  for (let i = 0; i < player.trail.length; i++) {
-      if (player.x === player.trail[i].x && player.y === player.trail[i].y) {
-          endGame();
-      }
-      // Draw trail
-      context.fillStyle = '#00f'; // Change this to the color you want for the trail
-      context.fillRect(player.trail[i].x, player.trail[i].y, player.width, player.height);
-  }
-
-  // Add new position to trail
-  player.trail.push({x: player.x, y: player.y});
-
-  // Draw player
-  context.fillStyle = player.color;
-  context.fillRect(player.x, player.y, player.width, player.height);
-
-  // Limit trail length
-  while (player.trail.length > 100) {
-      player.trail.shift();
-  }
+    drawPlayer(player1);
+    drawPlayer(player2);
 }
 
+// Update player position, check for game over, and update trail
+function updatePlayer(player, direction) {
+    player.x += player.dx;
+    player.y += player.dy;
 
-
-// Update player direction
-window.addEventListener('keydown', function(e) {
-    switch (e.key) {
-        case 'ArrowUp':
-            if (direction !== 'down') {
-                direction = 'up';
-                player.dy = -gridSize;
-                player.dx = 0;
-            }
-            break;
-        case 'ArrowDown':
-            if (direction !== 'up') {
-                direction = 'down';
-                player.dy = gridSize;
-                player.dx = 0;
-            }
-            break;
-        case 'ArrowLeft':
-            if (direction !== 'right') {
-                direction = 'left';
-                player.dy = 0;
-                player.dx = -gridSize;
-            }
-            break;
-        case 'ArrowRight':
-            if (direction !== 'left') {
-                direction = 'right';
-                player.dy = 0;
-                player.dx = gridSize;
-            }
-            break;
+    // Acceleration when close to wall
+    let distanceToWall;
+    switch (direction) {
+        case 'up': distanceToWall = player.y; break;
+        case 'down': distanceToWall = canvas.height - player.y; break;
+        case 'left': distanceToWall = player.x; break;
+        case 'right': distanceToWall = canvas.width - player.x; break;
     }
-});
+    if (distanceToWall < 100) {
+        player.speed = 50;
+    } else {
+        player.speed = 100;
+    }
 
-// End game
+    // Game over when player hits the edge
+    if (player.x < 0 || player.y < 0 || player.x > canvas.width || player.y > canvas.height) {
+        endGame();
+    }
 
-function endGame() {
-  clearInterval(gameLoop);
-  document.getElementById('start-game').style.display = 'block';
-  alert("Game Over!");
+    // Game over when player collides with their own trail or the other player's trail
+    for (let i = 0; i < player.trail.length; i++) {
+        if (player.x === player.trail[i].x && player.y === player.trail[i].y) {
+            endGame();
+        }
+    }
+    for (let i = 0; i < player2.trail.length; i++) {
+        if (player.x === player2.trail[i].x && player.y === player2.trail[i].y) {
+            endGame();
+        }
+    }
+
+    // Add new position to trail
+    player.trail.push({x: player.x, y: player.y});
+
+    // Limit trail length
+    while (player.trail.length > 100) {
+        player.trail.shift();
+    }
 }
 
-// Start game button
-document.getElementById('start-game').addEventListener('click', startGame);
+// Draw player and their trail
 
-// Call startGame to start the game
-startGame();
+    // Draw player and their trail
+    function drawPlayer(player) {
+      // Draw trail
+      for (let i = 0; i < player.trail.length; i++) {
+          context.fillStyle = player.color; // Trail color same as player color
+          context.fillRect(player.trail[i].x, player.trail[i].y, player.width, player.height);
+      }
+
+      // Draw player
+      context.fillStyle = player.color;
+      context.fillRect(player.x, player.y, player.width, player.height);
+  }
+
+  // Update player direction
+  window.addEventListener('keydown', function(e) {
+      switch (e.key) {
+          case 'ArrowUp':
+              if (direction1 !== 'down') {
+                  direction1 = 'up';
+                  player1.dy = -gridSize;
+                  player1.dx = 0;
+              }
+              break;
+          case 'ArrowDown':
+              if (direction1 !== 'up') {
+                  direction1 = 'down';
+                  player1.dy = gridSize;
+                  player1.dx = 0;
+              }
+              break;
+          case 'ArrowLeft':
+              if (direction1 !== 'right') {
+                  direction1 = 'left';
+                  player1.dy = 0;
+                  player1.dx = -gridSize;
+              }
+              break;
+          case 'ArrowRight':
+              if (direction1 !== 'left') {
+                  direction1 = 'right';
+                  player1.dy = 0;
+                  player1.dx = gridSize;
+              }
+              break;
+          case 'w':
+              if (direction2 !== 'down') {
+                  direction2 = 'up';
+                  player2.dy = -gridSize;
+                  player2.dx = 0;
+              }
+              break;
+          case 's':
+              if (direction2 !== 'up') {
+                  direction2 = 'down';
+                  player2.dy = gridSize;
+                  player2.dx = 0;
+              }
+              break;
+          case 'a':
+              if (direction2 !== 'right') {
+                  direction2 = 'left';
+                  player2.dy = 0;
+                  player2.dx = -gridSize;
+              }
+              break;
+          case 'd':
+              if (direction2 !== 'left') {
+                  direction2 = 'right';
+                  player2.dy = 0;
+                  player2.dx = gridSize;
+              }
+              break;
+      }
+  });
+
+  // End game
+  function endGame() {
+      clearInterval(gameLoop);
+      document.getElementById('start-game').style.display = 'block';
+      alert("Game Over!");
+  }
+
+  // Start game button
+  document.getElementById('start-game').addEventListener('click', startGame);
+
+  // Call startGame to start the game
+  startGame();
+
